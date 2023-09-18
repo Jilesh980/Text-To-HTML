a17a42626f677d42a1127a93348c5f5be58c6a8d
Jilesh980 <88798168+Jilesh980@users.noreply.github.com>
2023-09-18 23:52:49 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
+// Name: Jilesh Sanjaykumar Patel
+// Student id: 133261206
+
 // src/index.js
 const fs = require('fs');
 const path = require('path');
 const yargs = require('yargs');
 
 // Function to process a single .txt file and generate an HTML file
-function processFile(filePath) {
+function processFile(filePath, outputDir) {
   const fileName = path.basename(filePath, '.txt');
-  const outputFilePath = `til/${fileName}.html`;
+  const outputFilePath = path.join(outputDir, `${fileName}.html`);
   const fileContent = fs.readFileSync(filePath, 'utf8');
-  const paragraphs = fileContent.split('\n\n').map((paragraph) => `<p>${paragraph}</p>`).join('\n');
+
+  // Extract title and content
+  const [title, ...paragraphs] = fileContent.split('\n\n');
+  const titleHtml = title ? `<title>${title}</title><h1>${title}</h1>` : '';
+  const paragraphsHtml = paragraphs.map((paragraph) => `<p>${paragraph}</p>`).join('\n');
+  
   const htmlContent = `<!doctype html>
   <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>${fileName}</title>
+    ${titleHtml}
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
-    ${paragraphs}
+    ${paragraphsHtml}
   </body>
   </html>`;
 
@@ -26,11 +34,11 @@ function processFile(filePath) {
 }
 
 // Function to process a directory of .txt files
-function processDirectory(dirPath) {
+function processDirectory(dirPath, outputDir) {
   const files = fs.readdirSync(dirPath);
   files.forEach((file) => {
     if (file.endsWith('.txt')) {
-      processFile(path.join(dirPath, file));
+      processFile(path.join(dirPath, file), outputDir);
     }
   });
 }
@@ -40,27 +48,34 @@ yargs
   .scriptName('til')
   .usage('$0 <cmd> [args]')
   .command('process [input]', 'Process .txt file(s) and generate HTML', (yargs) => {
-    yargs.positional('input', {
-      describe: 'Input .txt file or folder',
-      type: 'string',
-    });
+    yargs
+      .positional('input', {
+        describe: 'Input .txt file or folder',
+        type: 'string',
+      })
+      .option('output', {
+        alias: 'o',
+        describe: 'Output directory',
+        type: 'string',
+      });
   }, (argv) => {
     const inputPath = argv.input;
+    const outputDir = argv.output || 'til'; // Use 'til' if not specified
 
-    // Create the 'til' directory (if it doesn't exist)
-    if (!fs.existsSync('til')) {
-      fs.mkdirSync('til');
+    // Create the output directory if it doesn't exist
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
     }
 
     // If it's a directory, process all .txt files within
     if (fs.lstatSync(inputPath).isDirectory()) {
-      processDirectory(inputPath);
+      processDirectory(inputPath, outputDir);
     } else {
-      processFile(inputPath);
+      processFile(inputPath, outputDir);
     }
   })
   .demandCommand(1, 'You need to specify a command.')
-  .version('v', 'Show the tool\'s name and version', `til v${packageJson.version}`)
+  .version('v', 'Show the tool\'s name and version', `til v1.0.0`)
   .alias('v', 'version')
   .help('h', 'Show a useful help message')
   .alias('h', 'help')
