69d6b9853cea191a5054d7b9bf1946fdf48656bc
Jilesh980 <jilesh9034@gmail.com>
2023-09-29 18:14:22 -0400

    Implementing and updating code features for issue #8

--- src/index.js
+++ src/index.js
@@ -1,55 +1,66 @@
 // src/index.js
 const fs = require("fs");
 const path = require("path");
+const { argv } = require("process");
 const yargs = require("yargs");
 
 // Function to process a single .txt or .md file and generate an HTML file
-function processFile(filePath, outputDir) {
-  const fileExt = path.extname(filePath);
-  const fileName = path.basename(filePath, fileExt);
-  const outputFilePath = path.join(outputDir, `${fileName}.html`);
-  const fileContent = fs.readFileSync(filePath, "utf8");
+function processFile(filePath, outputDir, lang) {
+  try {
+    const fileExt = path.extname(filePath);
+    const fileName = path.basename(filePath, fileExt);
+    const outputFilePath = path.join(outputDir, `${fileName}.html`);
+    const fileContent = fs.readFileSync(filePath, "utf8");
 
-  // Extract title and content
-  const [title, ...paragraphs] = fileContent.split(/\n{1,}/); // Split by one
-  const titleHtml = title ? `<title>${title}</title><h1>${title}</h1>` : "";
-  const paragraphsHtml = paragraphs
-    .map((paragraph) => {
-      // Support for bold text using double asterisks
-      paragraph = paragraph.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
-      return `<p>${paragraph}</p>`;
-    })
-    .join("\n");
+    // Extract title and content
+    const [title, ...paragraphs] = fileContent.split(/\n{1,}/); // Split by one
+    const titleHtml = title ? `<title>${title}</title><h1>${title}</h1>` : "";
+    const paragraphsHtml = paragraphs
+      .map((paragraph) => {
+        // Support for bold text using double asterisks
+        paragraph = paragraph.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
+        return `<p>${paragraph}</p>`;
+      })
+      .join("\n");
 
-  const htmlContent = `<!doctype html>
-  <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    ${titleHtml}
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
-  <body>
-    ${paragraphsHtml}
-  </body>
-  </html>`;
+    const htmlContent = `<!doctype html>
+    <html lang="${lang}">
+    <head>
+      <meta charset="utf-8">
+      ${titleHtml}
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+      ${paragraphsHtml}
+    </body>
+    </html>`;
 
-  fs.writeFileSync(outputFilePath, htmlContent);
-  console.log(`Generated ${outputFilePath}`);
+    fs.writeFileSync(outputFilePath, htmlContent);
+    console.log(`Generated ${outputFilePath}`);
+  } catch (error) {
+    console.error(`Error processing file: ${error.message}`);
+    process.exit(1); // Exit with a non-zero exit code
+  }
 }
 
 // Function to process a directory of .txt and .md files
-function processDirectory(dirPath, outputDir) {
-  const files = fs.readdirSync(dirPath);
-  files.forEach((file) => {
-    const filePath = path.join(dirPath, file);
-    if (fs.lstatSync(filePath).isDirectory()) {
-      processDirectory(filePath, outputDir);
-    } else if (file.match(/\.(txt|md)$/)) {
-      // Process only .txt and .md files
-      processFile(filePath, outputDir);
-      console.log(`Converted ${filePath} to HTML.`);
-    }
-  });
+function processDirectory(dirPath, outputDir, lang) {
+  try {
+    const files = fs.readdirSync(dirPath);
+    files.forEach((file) => {
+      const filePath = path.join(dirPath, file);
+      if (fs.lstatSync(filePath).isDirectory()) {
+        processDirectory(filePath, outputDir, lang);
+      } else if (file.match(/\.(txt|md)$/)) {
+        // Process only .txt and .md files
+        processFile(filePath, outputDir, lang);
+        console.log(`Converted ${filePath} to HTML.`);
+      }
+    });
+  } catch (error) {
+    console.error(`Error processing directory: ${error.message}`);
+    process.exit(1); // Exit with a non-zero exit code
+  }
 }
 
 // Main command-line tool logic
@@ -69,11 +80,18 @@ yargs
           alias: "o",
           describe: "Output directory",
           type: "string",
+        })
+        .option("lang", {
+          alias: "l",
+          describe: "language for lang attribute on <html> elements",
+          type: "string",
+          default: "en-CA",
         });
     },
     (argv) => {
       const inputPath = argv.input;
       const outputDir = argv.output || "til"; // Use 'til' if not specified
+      const lang = argv.lang;
 
       // Create the output directory if it doesn't exist
       if (!fs.existsSync(outputDir)) {
@@ -82,15 +100,16 @@ yargs
 
       // If it's a directory, process all .txt and .md files within
       if (fs.lstatSync(inputPath).isDirectory()) {
-        processDirectory(inputPath, outputDir);
+        processDirectory(inputPath, outputDir, lang);
       } else if (inputPath.match(/\.(txt|md)$/)) {
         // Process only .txt and .md files
-        processFile(inputPath, outputDir);
+        processFile(inputPath, outputDir, lang);
         console.log(`Converted ${inputPath} to HTML.`);
       } else {
         console.error(
           "Invalid input. Please provide a valid .txt or .md file or folder."
         );
+        process.exit(1); // Exit with a non-zero exit code
       }
     }
   )
